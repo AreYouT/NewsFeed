ff79c169003e34db67886c902d75350465730e01
tktcod <hs060100@gmail.com>
2024-02-07 16:01:48 +0900

    entity 추가 및 필터 구조화

---
+++ src/main/java/com/sparta/newsfeed/entity/Comment.java
@@ -0,0 +1,33 @@
+package com.sparta.newsfeed.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "comments")
+public class Comment extends Timestamped{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String contents;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    public Comment(String contents, User user, Post post) {
+        this.contents = contents;
+        this.user = user;
+        this.post = post;
+    }
+}
