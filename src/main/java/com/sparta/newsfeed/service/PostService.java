e14d3991c11cb3987dbee5dca3fbeb8011994367
kiwimel0n <alstkd8580@naver.com>
2024-02-08 12:28:56 +0900

    PostService method Transactional @Annotation 추가

--- src/main/java/com/sparta/newsfeed/service/PostService.java
+++ src/main/java/com/sparta/newsfeed/service/PostService.java
@@ -7,6 +7,7 @@ import com.sparta.newsfeed.entity.User;
 import com.sparta.newsfeed.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Comparator;
 import java.util.List;
@@ -17,12 +18,14 @@ public class PostService {
 
     private final PostRepository postRepository;
 
+    @Transactional
     public void savePost(PostRequestDto requestDto, User user) {
 
         postRepository.save(new Post(requestDto,user));
 
     }
 
+    @Transactional(readOnly = true)
     public List<PostListResponseDto> findByCategoryNameToList(String category) {
         return postRepository.findByCategory(category)
                 .stream()
