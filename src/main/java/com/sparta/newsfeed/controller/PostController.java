e58d3e3bc196b1e6138c2cffe0f5965ee99d7577
tangpoo <lhinav@naver.com>
2024-02-09 17:10:06 +0900

    PostResponseDto 삭제, ResponseDto 반환으로 변경

--- src/main/java/com/sparta/newsfeed/controller/PostController.java
+++ src/main/java/com/sparta/newsfeed/controller/PostController.java
@@ -33,15 +33,7 @@ public class PostController {
             @RequestBody PostRequestDto requestDto,
             @AuthenticationPrincipal UserDetailsImpl userDetails
     ){
-//        빌더 패턴 적용전 코드
-//        PostResponseDto responseDto = postService.savePost(requestDto, userDetails.getUser());
-//
-//        ResponseDto<PostResponseDto> form = new ResponseDto<>(HttpStatus.OK, "success", responseDto);
-//
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.setContentType(new MediaType("application", "json", StandardCharsets.UTF_8));
-//
-//        return new ResponseEntity<>(form, headers, HttpStatus.CREATED);
+
         return ResponseEntity.ok()
                 .body(ResponseDto.<PostResponseDto>builder()
                         .httpStatus(HttpStatus.OK.value())
@@ -64,39 +56,39 @@ public class PostController {
     }
 
     @GetMapping("/home")
-    public ResponseEntity<PostResponse<List<PostResponseDto>>> RecommendedPosts() {
+    public ResponseEntity<ResponseDto<List<PostResponseDto>>> RecommendedPosts() {
         List<Post> posts = postService.getRecommendedPosts();
         List<PostResponseDto> postResponseDtos = posts.stream()
                 .map(PostResponseDto::new)
                 .collect(Collectors.toList());
 
         return ResponseEntity.ok().body(
-                PostResponse.<List<PostResponseDto>>builder()
-                        .StatusCode(HttpStatus.OK.value())
+                ResponseDto.<List<PostResponseDto>>builder()
+                        .httpStatus(HttpStatus.OK.value())
                         .message("추천 순으로 전체 게시글이 조회되었습니다.")
-                        .date(postResponseDtos)
+                        .data(postResponseDtos)
                         .build()
         );
     }
 
     @GetMapping("/{category}/{id}")
-    public ResponseEntity<PostResponse<PostResponseDto>> getPostById(@PathVariable Long id) {
+    public ResponseEntity<ResponseDto<PostResponseDto>> getPostById(@PathVariable Long id) {
         try {
             Post post = postService.getPostById(id);
             PostResponseDto postResponseDto = new PostResponseDto(post);
 
             return ResponseEntity.ok().body(
-                    PostResponse.<PostResponseDto>builder()
-                            .StatusCode(HttpStatus.OK.value())
+                    ResponseDto.<PostResponseDto>builder()
+                            .httpStatus(HttpStatus.OK.value())
                             .message("선택한 게시글이 조회되었습니다.")
-                            .date(postResponseDto)
+                            .data(postResponseDto)
                             .build()
             );
         } catch (NoSuchElementException e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND)
                     .body(
-                            PostResponse.<PostResponseDto>builder()
-                                    .StatusCode(HttpStatus.NOT_FOUND.value())
+                            ResponseDto.<PostResponseDto>builder()
+                                    .httpStatus(HttpStatus.NOT_FOUND.value())
                                     .message("해당 ID에 대한 게시글을 찾을 수 없습니다.")
                                     .build()
                     );
